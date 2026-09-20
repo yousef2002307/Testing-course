5d27266e9aa5607d38671f3e579ce0251f4d3e2a
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2026-09-20 14:31:27 +0300

    add

--- index.php
+++ index.php
@@ -1,36 +1,34 @@
-<?php
+﻿<?php
 
+class SecurityService
+{
+    private array $attemptsLog = [];
 
-function groupAnagrams(array $strs) {
- $arr = [];
-$charMap = [
-    ""  =>   1,
-    "a" =>   2, "b" =>   3, "c" =>   5, "d" =>   7, "e" =>  11,
-    "f" =>  13, "g" =>  17, "h" =>  19, "i" =>  23, "j" =>  29,
-    "k" =>  31, "l" =>  37, "m" =>  41, "n" =>  43, "o" =>  47,
-    "p" =>  53, "q" =>  59, "r" =>  61, "s" =>  67, "t" =>  71,
-    "u" =>  73, "v" =>  79, "w" =>  83, "x" =>  89, "y" =>  97,
-    "z" => 101,
-];
+    public function isValidPassword(string $password): bool
+    {
+        $isValid = strlen($password) >= 8 && preg_match('/[0-9]/', $password);
 
-for($i =0; $i < count($strs); $i++){
-    $str = $strs[$i];
-    $strarr = str_split($str);
-    $counter = 1;
-    for($j=0; $j < count($strarr); $j++){
-        $counter *= $charMap[$strarr[$j]];
+        if (!$isValid) {
+            $this->attemptsLog[] = [
+                'time' => date('Y-m-d H:i:s'),
+                'status' => 'FAILED_VALIDATION'
+            ];
+            
+            // Send warning to security team
+            mail('sec@example.com', 'Failed validation attempt', 'Password failed checks');
+        }
+
+        return $isValid;
     }
-    if(!array_key_exists($counter,$arr)){
-        $arr[$counter] = [];
+
+    public function getLogs(): array
+    {
+        return $this->attemptsLog;
     }
-    array_push($arr[$counter],$str);
 }
 
- $finalarr = [];
-   foreach($arr as $item){$finalarr[] = $item;
-    }
-    return $finalarr;
-}
-$strs = ["eat", "tea", "tan", "ate", "nat", "bat","xyz","zyx"];
-print_r(groupAnagrams($strs));
-
+// Tests
+$sec = new SecurityService();
+var_dump($sec->isValidPassword('weak'));
+var_dump($sec->isValidPassword('strongPass123'));
+print_r($sec->getLogs());
